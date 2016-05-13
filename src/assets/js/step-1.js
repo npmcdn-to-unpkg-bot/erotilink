5f865c23cadfe9ddad5498bd36fc5a10241a9eb7
Adau <adrien.dau@gmail.com>
2016-05-13 10:18:55 +0200

    Add form to carousel to manage user

---
+++ src/assets/js/step-1.js
@@ -0,0 +1,22 @@
+(function($) {
+  // Sélection de l'utilisatrice dans le formulaire lors du défilement des photos
+  $('.js-carousel').on('slid.bs.carousel', function (e) {
+    $(e.relatedTarget).find('[name="user"]').prop('checked', true);
+  });
+
+  // Récupération des données de l'utilisatrice
+  $('.js-form-step-1').on('submit', function (e) {
+    e.preventDefault();
+
+    var user = $(this).find('[name="user"]:checked').val();
+    user = $.parseJSON(user);
+
+    // Remplissage des données de l'utilisatrice de l'étape 2
+    $('.js-user-picture').attr('src', 'dist/assets/img/photos/' + user.picture);
+    $('.js-user-name').text(user.name);
+    $('.js-user-age').text(user.age);
+
+    // Passage à l'étape suivante
+    $('.step-1, .step-2').toggleClass('hidden');
+  });
+})(jQuery);
